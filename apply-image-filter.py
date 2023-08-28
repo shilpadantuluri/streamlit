a6b8346b65882db8b82785a4c06494243d8d8962
shilpadantuluri <143417071+shilpadantuluri@users.noreply.github.com>
2023-08-28 11:31:35 -0400

    Update apply-image-filter.py

--- apply-image-filter.py
+++ apply-image-filter.py
@@ -4,4 +4,5 @@ import altair
 from altair.vegalite.v4.api import Chart
 
 st.title('PicPlay: Explore Creativity with Interactive Image Filters')
-st.write('PicPlay: Explore Creativity with Interactive Image Filters')
+
+st.write("PicPlay: New Style Display")
